03a684c56788233c2cddc837ad14b07684b82b03
PatrickMorrell <43747743+PatrickMorrell@users.noreply.github.com>
2026-07-27 12:10:19 +0000

    Deploying to git-pages from @ PatrickMorrell/Regicide@a310dd2aa82e76598e970a3c0b92ebda337f4f3e 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: /YRn7gkj */
+/* Manifest version: kvYtXVik */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
